0e7dd82114ca9c7be877440f4534449f67429035
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-04 13:01:13 -0500

    [Update] - Enable the submit button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   
+  const validador = {
+    email: '',
+    asunto: '',
+    mensaje: ''
+  }
   //Seleccionar elementos de la interfaz.
   const inputMail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
+  const btnSubmit = document.querySelector('#formulario button[type="submit"]');
 
   //Asignacion de eventos.
-  inputMail.addEventListener('blur', validar);
-  inputAsunto.addEventListener('blur', validar);
-  inputMensaje.addEventListener('blur', validar);
+  inputMail.addEventListener('input', validar);
+  inputAsunto.addEventListener('input', validar);
+  inputMensaje.addEventListener('input', validar);
   
   //Validacion del evento.
   function validar(e) {
@@ -18,15 +24,26 @@ document.addEventListener('DOMContentLoaded', function () {
     //Verificamos si no hay algo escrito o tiene espacios vacios.
     if (e.target.value.trim() === '') {
       mostrarAlerta(`El campo ${e.target.id} es obligatorio.`, itemSeleccionado);
+      validador[e.target.name] = '';
+      comprobarValidaciones();
       return;
     }
     
+    //Validacion de e-mail.
     if (e.target.id === 'email' && !validarEmail(e.target.value)) {
       mostrarAlerta(`El email no es valido`, itemSeleccionado);
+      validador[e.target.name] = '';
+      comprobarValidaciones();
       return;
     }
 
     limpiarAlerta(itemSeleccionado);
+
+    //Asignar valores al objeto email.
+    validador[e.target.name] = e.target.value.trim().toLowerCase();
+    
+    //Comprobar validaciones.
+    comprobarValidaciones();
   }
 
   //Generacion de alerta de error en HTML.
@@ -49,10 +66,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  //Validacion de email.
+  //Expresion regular para validacion de email.
   function validarEmail( email ) {
     const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
     const resultado = regex.test(email);
     return resultado;
+  }
+
+  //Comprobacion de campos y validaciones.
+  function comprobarValidaciones() {
+    if (Object.values(validador).includes('')) {
+      btnSubmit.classList.add('opacity-50');
+      btnSubmit.disabled = true;
+      return;
+    } 
+    btnSubmit.classList.remove('opacity-50');
+    btnSubmit.disabled = false;
   }
 });
