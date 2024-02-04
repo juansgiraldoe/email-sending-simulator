17c81599713f649f220485ad182e144410ad61a4
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-04 11:55:19 -0500

    [Update] - Show and hide alerts based on the form field.

--- js/app.js
+++ js/app.js
@@ -12,11 +12,34 @@ document.addEventListener('DOMContentLoaded', function () {
   
   //Validacion del evento.
   function validar(e) {
+    //Seleccion del elemento padre.
+    const itemSeleccionado = e.target.parentElement;
+
     //Verificamos si no hay algo escrito o tiene espacios vacios.
     if (e.target.value.trim() === '') {
-      console.log('Debes llenar el campo');
-    } else {
-      console.log(e.target.value);
+      mostrarAlerta(`El campo ${e.target.id} es obligatorio.`, itemSeleccionado);
+      return;
+    }
+    limpiarAlerta(itemSeleccionado);
+  }
+
+  //Generacion de alerta de error en HTML.
+  function mostrarAlerta(mensaje, ref) {
+
+    limpiarAlerta(ref);
+    
+    const error = document.createElement('P')
+    error.textContent = mensaje;
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'rounded-lg');
+    
+    ref.appendChild(error)
+  }
+  
+  //Comprobar si ha hay alertas.
+  function limpiarAlerta(ref) {
+    const alerta = ref.querySelector('.bg-red-600')
+    if (alerta) {
+      alerta.remove();
     }
   }
 });
